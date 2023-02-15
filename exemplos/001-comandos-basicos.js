68e39b51bf6652d4560f7dbaaf43e138bbfd4480
mauv26 <mauro_lanza.souza@hotmail.com>
2023-02-14 21:23:30 -0300

    Variaveis

---
+++ exemplos/001-comandos-basicos.js
@@ -0,0 +1,23 @@
+console.log('Hello Word!');
+
+//Variaveis
+let nome        = 'Mauro'; 
+let idade       = 22;
+let altura      = 1.76;
+let trabalha    = true;
+let corFavorita = 'Azul';
+let cores_favoritas = ['preto','azul']
+let endereco = {
+    'logradouro' : 'Rua',
+    'nome_log'   : 'Brasil',
+    'n'          : '105 A',
+    'bairro'     : 'centro'
+};
+
+//Imprimir tipo de variavel
+console.log('Variável nome = '            + typeof(nome));
+console.log('Variável idade = '           + typeof(idade));
+console.log('Variável altura = '          + typeof(altura));
+console.log('Variável trabalha = '        + typeof(trabalha));
+console.log('Variável corFavorita = '     + typeof(corFavorita));
+console.log('Variável cores_favoritas = ' + typeof(cores_favoritas));
